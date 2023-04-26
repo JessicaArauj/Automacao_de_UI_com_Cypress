c92330a7e1f5544ffbcc9eb91239672c84c50d7f
Jessica Teixeira Araujo <111321791+JessicaTeixeiraAraujo@users.noreply.github.com>
2023-04-26 09:29:21 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -42,4 +42,22 @@ Cypress.Commands.add('addProdutos', (produto, tamanho, cor, quantidade) => {
     // cy.get('.woocommerce-message').should('contain', quantidade, 'x', produto, 'foram adicionados ao seu carrinho.') não funciona para somente 1 unidade
     cy.get('.woocommerce-message').should('contain', produto, 'foram adicionados ao seu carrinho.') // aceito para toda quantidade adquerida do item
     cy.get('#primary-menu > .menu-item-629 > a').click()
+});
+
+Cypress.Commands.add('preencherCheckout', () => {
+    cy.get('#billing_first_name').clear().type(faker.name.firstName())
+    cy.get('#billing_last_name').clear().type(faker.name.lastName())
+    cy.get('#billing_company').clear().type(faker.company.bs())
+    cy.get('#select2-billing_country-container').type('Brasil').click() // Pais
+    cy.get('#billing_address_1').clear().type(faker.address.city())
+    cy.get('#billing_address_2').clear().type('000')
+    cy.get('#billing_city').clear().type(faker.address.cityName())
+    cy.get('#select2-billing_state-container').type('São Paulo').click() // Estado
+    cy.get('#billing_postcode').clear().type('66065112')
+    cy.get('#billing_phone').clear().type(faker.phone.number('+48 91 ### ## ##'))
+    cy.get('#billing_email').clear().type(faker.internet.email())
+    cy.get('#payment_method_cod').check()
+    cy.get('#terms').check()
+    cy.get('#place_order').click()
 })
+     
